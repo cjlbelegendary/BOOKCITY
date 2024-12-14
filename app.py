2c77a7beb40b29e9c28db8ee2f04f85c95d36c15
cjlbelegendary <1325129305@qq.com>
2024-12-14 12:35:10 +0800

    删除了临时文件sql_flask.py

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 import ast
-from flask import Flask, render_template, redirect, url_for,request,flash
+from flask import Flask, render_template, redirect, url_for, request, flash
 import pymysql
 from sql_init import Mysql
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'mykey'
+
+
 def get_db_connection():
     db = pymysql.connect(host='localhost',
-                     user='root',
-                     password='root',
-                     database='bookstore',
-                     charset='utf8')
+                         user='root',
+                         password='root',
+                         database='bookstore',
+                         charset='utf8')
     print('连接数据库成功')
     return db
-@app.route('/')#登录页
+
+
+@app.route('/')  # 登录页
 def index():
     return render_template('login.html')
+
+
 @app.route('/login_fail')
 def login_fail():
     return render_template('login_fail.html')
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -52,11 +59,14 @@ def login():
             flash('用户名或密码错误')
             return redirect(url_for('login_fail'))
     return render_template('login.html')
+
+
 @app.route('/register_success')
 def register_success():
     return render_template('register_success.html')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -70,7 +80,8 @@ def register():
         if user:
             return render_template('register_fail.html')
         else:
-            cursor.execute('INSERT INTO users (username,password,type) VALUES(%s, %s, %s)',(username,password,usertype))
+            cursor.execute('INSERT INTO users (username,password,type) VALUES(%s, %s, %s)',
+                           (username, password, usertype))
         conn.commit()
         cursor.close()
         conn.close()
@@ -78,13 +89,16 @@ def register():
         # return redirect(url_for('/login'))
     return render_template('login.html')
 
-@app.route('/admin')#登录页
+
+@app.route('/admin')  # 登录页
 def admin():
     return render_template('admin.html')
 
-@app.route('/user')#登录页
+
+@app.route('/user')  # 登录页
 def user():
     return render_template('user.html')
+
 
 @app.route('/turn_to_register')
 def turn_to_register():
@@ -98,12 +112,14 @@ def info():
     results = db.getdata()
     return render_template("admin.html", results=results)
 
+
 @app.route("/info_user", methods=['GET', 'POST'])
 def info_user():
     # 调用
     db = Mysql()
     results = db.getdata()
     return render_template("user.html", results=results)
+
 
 @app.route("/search", methods=['POST'])
 def search():
@@ -122,6 +138,7 @@ def search():
             print('not found')
             print(results)
             return render_template("admin_notfound.html")
+
 
 @app.route("/search_user", methods=['POST'])
 def search_user():
@@ -146,10 +163,11 @@ def search_user():
 def back_to_admin():
     return render_template('admin.html')
 
+
 @app.route("/delete", methods=['POST'])
 def delete():
     if request.method == 'POST':
-        ID=request.form['id']
+        ID = request.form['id']
         print(ID)
         db = Mysql()
         results = db.delete(ID)
@@ -158,6 +176,7 @@ def delete():
             return redirect(url_for('info'))
         else:
             return render_template('delete_fail.html')
+
 
 @app.route("/update_by_id", methods=['POST'])
 def update_by_id():
@@ -174,29 +193,30 @@ def update_by_id():
         results = db.search_by_id(ID)
         print(results)
         print(results[0])
-        return render_template('update_main.html',results=results)
+        return render_template('update_main.html', results=results)
+
 
 @app.route("/update", methods=['POST'])
 def update():
     if request.method == 'POST':
-        id=request.form['id']
+        id = request.form['id']
         ID = ast.literal_eval(id)
         img = request.form['img']
-        print('img:'+img)
+        print('img:' + img)
         name = request.form['name']
-        print('name:'+name)
+        print('name:' + name)
         author = request.form['author']
-        print('author:'+author)
+        print('author:' + author)
         publish = request.form['publish']
-        print('publish:'+publish)
+        print('publish:' + publish)
         price = request.form['price']
-        print('price:'+price)
+        print('price:' + price)
         amount = request.form['amount']
-        print('amount:'+amount)
+        print('amount:' + amount)
         position = request.form['position']
-        print('position:'+position)
+        print('position:' + position)
         intro = request.form['intro']
-        print('intro:'+intro)
+        print('intro:' + intro)
         data = [ID, img, name, author, publish, price, amount, position, intro]
         print(data)
         db = Mysql()
@@ -205,14 +225,16 @@ def update():
         if result == True:
             results = db.search_by_id(ID)
             print(results)
-            return render_template('update_main.html',results=results)
+            return render_template('update_main.html', results=results)
         else:
             print('update fail')
             return render_template('update_fail.html')
 
+
 @app.route("/turn_to_insert")
 def turn_to_insert():
     return render_template("insert.html")
+
 
 @app.route("/insert", methods=['POST'])
 def insert():
@@ -242,12 +264,13 @@ def insert():
         if results == False:
             return render_template('insert_fail.html')
         else:
-            return render_template('insert_success.html',results=results)
+            return render_template('insert_success.html', results=results)
+
 
 @app.route("/back_to_insert")
 def back_to_insert():
     return render_template('insert.html')
 
-
 if __name__ == '__main__':
-    app.run()
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
--- sql_init.py
+++ sql_init.py
@@ -1,5 +1,4 @@
 import ast
-
 import pymysql
 class Mysql(object):
     def __init__(self):
@@ -9,45 +8,46 @@ class Mysql(object):
                 user="root",
                 password="root",
                 database="bookstore")
-            #游标对象
+            # 游标对象
             self.cursor = self.db.cursor()
             print("连接成功！")
         except:
             print("连接失败！")
- 	#查询数据函数
+
+    # 查询数据函数
     def getdata(self):
         sql = "select * from books order by id"
-        #执行sql语句
+        # 执行sql语句
         self.cursor.execute(sql)
-        #获取所有的记录
+        # 获取所有的记录
         results = self.cursor.fetchall()
         return results
 
     def search(self, find):
-        print('find='+find)
-        args='%'+find+'%'
+        print('find=' + find)
+        args = '%' + find + '%'
         sql = "SELECT * FROM books WHERE CONCAT(ID,书名,作者,出版社,价格,数量,位置,简介) like '%s'" % args
-        print('sql='+sql)
-        #执行sql语句
+        print('sql=' + sql)
+        # 执行sql语句
         self.cursor.execute(sql)
-        #获取所有的记录
+        # 获取所有的记录
         results = self.cursor.fetchall()
         return results
 
-    def search_by_id(self,id):
-        sql="SELECT * FROM books WHERE ID='%s'" % id
+    def search_by_id(self, id):
+        sql = "SELECT * FROM books WHERE ID='%s'" % id
         self.cursor.execute(sql)
         results = self.cursor.fetchall()
         return results
 
     def delete(self, id):
-        print('id='+id)
+        print('id=' + id)
         # data_tuple = ast.literal_eval(id)
         # ID=data_tuple[0]
         # print('ID='+ID)
         try:
             sql = "DELETE FROM books WHERE ID='%s'" % id
-            print('sql='+sql)
+            print('sql=' + sql)
             self.cursor.execute(sql)
             self.db.commit()
             return True
@@ -55,17 +55,17 @@ class Mysql(object):
             return False
 
     def update(self, data):
-        head = ['图片','书名','作者','出版社','价格','数量','位置','简介']
+        head = ['图片', '书名', '作者', '出版社', '价格', '数量', '位置', '简介']
         try:
             print(len(head))
             for i in range(len(head)):
-                if len(data[i+1])==0:
+                if len(data[i + 1]) == 0:
                     continue
                 print('i:')
                 print(i)
-                sql = "UPDATE books SET "+head[i]+"="+"'"+ data[i+1]+"'"
+                sql = "UPDATE books SET " + head[i] + "=" + "'" + data[i + 1] + "'"
                 sql = sql + ("WHERE ID='%s'" % data[0])
-                print('sql='+sql)
+                print('sql=' + sql)
                 self.cursor.execute(sql)
                 self.db.commit()
             return True
@@ -73,40 +73,40 @@ class Mysql(object):
             return False
 
     def insert(self, data):
-        head = ['ID','图片','书名','作者','出版社','价格','数量','位置','简介']
+        head = ['ID', '图片', '书名', '作者', '出版社', '价格', '数量', '位置', '简介']
         try:
             sql = "SELECT MAX(CAST(ID AS UNSIGNED)) FROM books"
             print(sql)
             self.cursor.execute(sql)
             max_id = self.cursor.fetchone()
             print(max_id)
-            if(max_id[0]==None):
+            if (max_id[0] == None):
                 max_id = int(1)
             else:
-                max_id = int(max_id[0])+1
-            print('max_id='+str(max_id))
+                max_id = int(max_id[0]) + 1
+            print('max_id=' + str(max_id))
             max_id = str(max_id)
             data.insert(0, max_id)
-            sql="INSERT INTO books ("
+            sql = "INSERT INTO books ("
 
             for i in range(len(head)):
-                if len(data[i])==0:
+                if len(data[i]) == 0:
                     continue
                 if i == 0:
                     sql = sql + head[i]
                 else:
-                    sql = sql + ','+head[i]
-            sql= sql+(") VALUES (")
+                    sql = sql + ',' + head[i]
+            sql = sql + (") VALUES (")
 
             for i in range(len(data)):
-                if len(data[i])==0:
+                if len(data[i]) == 0:
                     continue
                 if i == 0:
                     sql = sql + "'" + data[i] + "'"
                 else:
                     sql = sql + ",'" + data[i] + "'"
-            sql = sql+")"
-            print('sql='+sql)
+            sql = sql + ")"
+            print('sql=' + sql)
             self.cursor.execute(sql)
             self.db.commit()
             results = self.search_by_id(max_id)
@@ -114,9 +114,6 @@ class Mysql(object):
         except Exception as e:
             return False
 
-
-
-
-    #关闭
+    # 关闭
     def __del__(self):
         self.db.close()
